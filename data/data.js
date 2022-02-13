b4b8bf88096867725b4be52462d2844c02a2f7a4
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-02-14 00:48:59 +0900

    json

--- data/data.js
+++ data/data.js
@@ -1,12 +1,16 @@
-fetch("http://localhost:3000/users")
+fetch("http://localhost:3000/users", { method: "GET" })
   .then((response) => {
-    //     const data = response.json();
+    const data = response.json();
     //     console.log(data);
-    return response.json();
+    return data;
   })
   .then((data) => {
-    console.log(data[0].name);
-    const html = data.data.map();
-    document.querySelector("#test").innerHTML = `<h1>${data[0].name}</h1>`;
+    console.log(data);
+    const html = data.map((user) => {
+      return `<p>id: ${user.id} name: ${user.name}</p>`;
+    });
+    console.log(html);
+    document.getElementById("test").innerHTML = `<h1>${html}</h1>`;
+    //or  .insertAdjacentHTML("afterbegin", `<h1>${html}</h1>`);
   })
   .catch((err) => console.log("Request Failed", err));
